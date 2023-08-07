9596577cf3c41f9a0fa22b6bcbf4dbfce376426e
lsakee <93514333+lsakee@users.noreply.github.com>
2023-08-08 03:30:33 +0900

    기초세팅 core-ui#1

--- core-ui/build.gradle.kts
+++ core-ui/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    kotlin("android")
+    kotlin("kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -31,14 +33,25 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
+    }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // Lifecycle Ktx
+    implementation(AndroidX.LIFECYCLE_RUNTIME)
+
+    // Matrial Design
+    implementation(Google.MATERIAL)
+
+    //Hilt
+    implementation(Google.HILT_ANDROID)
+    kapt(Google.HILT_ANDROID_COMPILER)
+    // Test Dependency
+    androidTestImplementation(TestDependencies.EXT_JUNIT)
+    androidTestImplementation(TestDependencies.ESPRESSO_CORE)
+    testImplementation(TestDependencies.JUNIT)
 }
