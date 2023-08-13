7f4cdc74d6d5cd3fd09236cfd971ee09261ca9d2
lsakee <93514333+lsakee@users.noreply.github.com>
2023-08-13 17:48:45 +0900

    [FEAT] 로그인 auth code#3

--- presentation/src/main/java/com/velogm/presentation/ui/SignInActivity.kt
+++ presentation/src/main/java/com/velogm/presentation/ui/SignInActivity.kt
@@ -12,6 +12,7 @@ import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.common.api.Scope
 import com.google.android.gms.tasks.Task
 import com.velogm.presentation.BuildConfig
+import com.velogm.presentation.BuildConfig.CLIENT_ID
 import com.velogm.presentation.databinding.ActivitySignInBinding
 import timber.log.Timber
 
@@ -25,16 +26,15 @@ class SignInActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         initView()
     }
 
     private fun initView() {
         getGoogleClient()
-
         googleSignResultLauncher = registerForActivityResult(
             ActivityResultContracts.StartActivityForResult()
         ) { result ->
+            Timber.tag("test").d("${result.resultCode}")
             val task: Task<GoogleSignInAccount> =
                 GoogleSignIn.getSignedInAccountFromIntent(result.data)
             handleSignInResult(task)
@@ -45,13 +45,11 @@ class SignInActivity : AppCompatActivity() {
         binding.btnSignInGoogle.setOnClickListener {
             val googleSignInOption =
                 GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                    .requestScopes(Scope("https://www.googleapis.com/auth/pubsub"))
-                    .requestServerAuthCode(BuildConfig.CLIENT_ID)
-                    .requestEmail()
+                    .requestServerAuthCode(CLIENT_ID)
                     .build()
             val mGoogleSignInClient = GoogleSignIn.getClient(this, googleSignInOption)
 
-            var signIntent: Intent = mGoogleSignInClient.getSignInIntent()
+            var signIntent: Intent = mGoogleSignInClient.signInIntent
             googleSignResultLauncher.launch(signIntent)
         }
     }
@@ -65,7 +63,6 @@ class SignInActivity : AppCompatActivity() {
             Timber.d(email)
             Timber.d(googletoken)
             Timber.d(googleTokenAuth)
-
         } catch (e: ApiException) {
             Timber.d("signInResult:failed Code = " + e.statusCode)
         }
