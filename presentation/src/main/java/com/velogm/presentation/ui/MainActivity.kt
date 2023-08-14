601a30c31dc3ea96b3f2aee99baff5cd04600343
lsakee <93514333+lsakee@users.noreply.github.com>
2023-08-15 00:58:53 +0900

    [FEAT] : nav 연동 #12

--- presentation/src/main/java/com/velogm/presentation/ui/MainActivity.kt
+++ presentation/src/main/java/com/velogm/presentation/ui/MainActivity.kt
@@ -1,17 +1,48 @@
 package com.velogm.presentation.ui
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.velogm.core_ui.base.BindingActivity
 import com.velogm.presentation.R
+import com.velogm.presentation.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        //test
+        initView()
+    }
 
+   private fun initView() {
+        val navController =
+            supportFragmentManager.findFragmentById(R.id.fc_main)?.findNavController()!!
+
+        with(binding) {
+            botNavMain.itemIconTintList = null
+            navController?.let { NavController ->
+                botNavMain.setupWithNavController(NavController)
+            }
+
+        }
+
+        setBottomVisible(navController)
+    }
+
+    private fun setBottomVisible(navController: NavController) {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.botNavMain.visibility = if (destination.id in listOf(
+                    R.id.navigation_home,
+                    R.id.navigation_bookmark,
+                    R.id.navigation_setting,
+                    R.id.navigation_follow
+                )
+            ) View.VISIBLE else View.GONE
+
+        }
     }
 }
