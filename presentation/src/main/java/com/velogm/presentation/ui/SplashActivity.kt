f6be95846734839da701819bb4f2578ff4c6035c
Sohyun <so405@naver.com>
2023-08-15 18:09:02 +0900

    [FEAT] : splash / #5

---
+++ presentation/src/main/java/com/velogm/presentation/ui/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.velogm.presentation.ui
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, SignInActivity::class.java))
+        finish()
+    }
+}
