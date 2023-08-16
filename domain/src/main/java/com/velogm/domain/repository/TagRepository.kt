fb8d1f8ecbc9ab66c7579e52e9a4d2d78fe278d5
lsakee <93514333+lsakee@users.noreply.github.com>
2023-08-17 03:11:02 +0900

    [FEAT] : Add-tag 도메인 계층 서버통신구현 #18 get tag / popular

--- domain/src/main/java/com/velogm/domain/repository/TagRepository.kt
+++ domain/src/main/java/com/velogm/domain/repository/TagRepository.kt
@@ -3,6 +3,8 @@ package com.velogm.domain.repository
 import com.velogm.domain.model.Tag
 import kotlinx.coroutines.flow.Flow
 
-interface HomeRepository {
+interface TagRepository {
     suspend fun getTag(): Flow<List<Tag>>
+    suspend fun getPopularTag():Flow<List<Tag>>
+
 }
