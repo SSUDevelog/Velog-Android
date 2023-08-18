ecc69049da007145c28ce37f98b28f017c443584
lsakee <93514333+lsakee@users.noreply.github.com>
2023-08-19 07:46:56 +0900

    [FIX] : tab-스크롤 안됨 현상 수정#18

---
+++ presentation/src/main/java/com/velogm/presentation/util/Debouncer.kt
@@ -0,0 +1,14 @@
+package com.velogm.presentation.util
+
+import kotlinx.coroutines.*
+
+class Debouncer<T> {
+    private var debounceJob: Job? = null
+    fun setDelay(value: T, delay: Long, action: (T) -> Unit) {
+        debounceJob?.cancel()
+        debounceJob = GlobalScope.launch(Dispatchers.Main) {
+            delay(delay)
+            action(value)
+        }
+    }
+}
