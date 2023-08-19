3639b6164bd36f50c2c6d657ffeec1d0059bbff6
lsakee <93514333+lsakee@users.noreply.github.com>
2023-08-20 08:28:25 +0900

    [FEAT] : getTagPost data #21 repoImpl / datasource/impl /service

--- data/src/main/java/com/velogm/data/datasource/TagDataSource.kt
+++ data/src/main/java/com/velogm/data/datasource/TagDataSource.kt
@@ -1,8 +1,13 @@
 package com.velogm.data.datasource
 
+import com.velogm.data.dto.response.PostListDto
+import com.velogm.data.dto.response.TrendPostDto
+
 interface TagDataSource {
     suspend fun getTag(): List<String>
     suspend fun getPopularTag(): List<String>
     suspend fun deleteTag(tag:String): String
     suspend fun addTag(tag:String): String
+
+    suspend fun getTagPosts(tag:String) : List<TrendPostDto>
 }
