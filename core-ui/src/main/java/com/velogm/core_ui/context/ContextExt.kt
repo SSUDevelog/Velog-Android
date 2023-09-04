8379f17db01b7cade28d814ff1420850e7d9bbdb
Sohyun <so405@naver.com>
2023-09-04 22:45:42 +0900

    [FEAT] : dialog 사이즈 조절 확장 함수 / #19

--- core-ui/src/main/java/com/velogm/core_ui/context/ContextExt.kt
+++ core-ui/src/main/java/com/velogm/core_ui/context/ContextExt.kt
@@ -2,13 +2,16 @@ package com.velogm.core_ui.context
 
 import android.app.Activity
 import android.content.Context
+import android.os.Build
 import android.view.View
+import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.DialogFragment
 import com.google.android.material.snackbar.Snackbar
 
 fun Context.toast(message: String) {
@@ -32,4 +35,30 @@ fun Context.drawableOf(@DrawableRes resId: Int) = ContextCompat.getDrawable(this
 fun Context.hideKeyboard(view: View) {
     val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
     inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+}
+
+fun Context.dialogFragmentResize(dialogFragment: DialogFragment, width: Float, height: Float) {
+    val windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+
+    if (Build.VERSION.SDK_INT < 30) {
+
+        val display = applicationContext.resources.displayMetrics
+
+        val window = dialogFragment.dialog?.window
+
+        val x = display.widthPixels
+        val y = display.heightPixels
+        window?.setLayout(x, y)
+
+    } else {
+
+        val rect = windowManager.currentWindowMetrics.bounds
+
+        val window = dialogFragment.dialog?.window
+
+        val x = (rect.width() * width).toInt()
+        val y = (rect.height() * height).toInt()
+
+        window?.setLayout(x, y)
+    }
 }
