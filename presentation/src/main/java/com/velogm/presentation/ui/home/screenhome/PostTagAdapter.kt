57e0c168a025fc2560f0e7bc9fb0a1ae67659698
lsakee <93514333+lsakee@users.noreply.github.com>
2023-09-18 23:44:15 +0900

    [FEAT] post tag rv 연결 #33

---
+++ presentation/src/main/java/com/velogm/presentation/ui/home/screenhome/PostTagAdapter.kt
@@ -0,0 +1,34 @@
+package com.velogm.presentation.ui.home.screenhome
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import com.velogm.core_ui.view.ItemDiffCallback
+import com.velogm.presentation.databinding.ItemPostTagBinding
+import com.velogm.presentation.model.TagModel
+import com.velogm.presentation.ui.addtag.viewholder.MyTagViewHolder
+import com.velogm.presentation.ui.home.screenhome.viewholder.MyPostTagViewHolder
+
+class PostTagAdapter(
+) : ListAdapter<TagModel, MyPostTagViewHolder>(
+    MyTagDiffCalback,
+) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyPostTagViewHolder {
+        val binding =
+            ItemPostTagBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyPostTagViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MyPostTagViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    companion object {
+        private val MyTagDiffCalback =
+            ItemDiffCallback<TagModel>(
+                onItemsTheSame = { old, new -> old.tag == new.tag },
+                onContentsTheSame = { old, new -> old == new }
+            )
+    }
+}
--- presentation/src/main/java/com/velogm/presentation/ui/home/screenhome/viewholder/PostViewHolder.kt
+++ presentation/src/main/java/com/velogm/presentation/ui/home/screenhome/viewholder/PostViewHolder.kt
@@ -4,8 +4,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.velogm.presentation.databinding.ItemRvPostBinding
 import com.velogm.presentation.model.PostModel
 import com.velogm.presentation.model.TagModel
-import com.velogm.presentation.ui.addtag.adapter.AddTagAdapter
-import timber.log.Timber
+import com.velogm.presentation.ui.home.screenhome.PostTagAdapter
 
 class PostViewHolder(
     private val binding: ItemRvPostBinding,
@@ -16,10 +15,9 @@ class PostViewHolder(
         binding.root.setOnClickListener {
             postClick(data)
         }
-        Timber.tag("test").d(data.tag.map { TagModel(it) }.toString())
-        val addTagAdapter = AddTagAdapter()
-        binding.rvPostTagList.adapter = addTagAdapter
-        addTagAdapter.submitList(data.tag.map { TagModel(it) })
+        val postTagAdapter = PostTagAdapter()
+        binding.rvPostTagList.adapter = postTagAdapter
+        postTagAdapter.submitList(data.tag.map { TagModel(it) })
         binding.executePendingBindings()
     }
 }
