608cd9d6bb4ba9cb59913b34b7e802d29a8c5eb4
lsakee <93514333+lsakee@users.noreply.github.com>
2023-10-09 23:44:37 +0900

    [FEAT] : webVIew viewmodel 추가 #40

--- presentation/src/main/java/com/velogm/presentation/ui/webview/WebViewActivity.kt
+++ presentation/src/main/java/com/velogm/presentation/ui/webview/WebViewActivity.kt
@@ -1,15 +1,21 @@
 package com.velogm.presentation.ui.webview
 
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.fragment.app.viewModels
 import com.velogm.core_ui.base.BindingActivity
 import com.velogm.presentation.R
 import com.velogm.presentation.databinding.ActivityWebviewBinding
+import com.velogm.presentation.ui.follow.AddFollowerViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
 class WebViewActivity :
     BindingActivity<ActivityWebviewBinding>(R.layout.activity_webview) {
+
+
+    private val webViewViewModel by viewModels<WebviewViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +33,7 @@ class WebViewActivity :
 //        }
 //
 //        binding.webviewFollowBtn.setOnClickListener {
-//            it.isSelected=true
+//            webViewViewModel.addFollower()
 //        }
     }
 
