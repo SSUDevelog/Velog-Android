d5da197106022df7931fc2a49513f823f9d9bf5b
lsakee <93514333+lsakee@users.noreply.github.com>
2023-10-09 23:39:35 +0900

    [FEAT] : add follow viewmodel 추가 #40

---
+++ presentation/src/main/java/com/velogm/presentation/ui/webview/WebviewViewModel.kt
@@ -0,0 +1,52 @@
+package com.velogm.presentation.ui.webview
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
+import com.velogm.core_ui.view.UiState
+import com.velogm.domain.model.InputFollower
+import com.velogm.domain.usecase.AddFollowerUseCase
+import com.velogm.domain.usecase.GetInputFollowerUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import javax.inject.Inject
+
+@HiltViewModel
+class WebviewViewModel @Inject constructor(
+    private val addFollowerUseCase: AddFollowerUseCase
+) : ViewModel() {
+
+    private val _eventData = MutableSharedFlow<UiState<String>>()
+    val eventData: SharedFlow<UiState<String>> = _eventData.asSharedFlow()
+
+    private var fcmToken: String = ""
+
+    init {
+        getFcmToken()
+    }
+
+    fun addFollower(followerName: String) = viewModelScope.launch {
+        addFollowerUseCase(followerName, fcmToken).collect {
+            _eventData.emit(UiState.Success(it))
+        }
+    }
+
+    private fun getFcmToken() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener {
+            if (!it.isSuccessful) {
+                Timber.d("fcm registration token failed")
+                return@OnCompleteListener
+            }
+            fcmToken = it.result
+            Timber.tag("fcmtoken").d(it.result.toString())
+        })
+    }
+}
